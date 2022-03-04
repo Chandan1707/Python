653e0c14f849cc5a4bac5d3c29316134bf8073b4
chandan <chandan743247@gmail.com>
2022-03-04 23:18:44 +0530

    vioel finding code are added

--- Regular_Ex.py
+++ Regular_Ex.py
@@ -1,7 +1,14 @@
 import re
 
 string = 'hello i am chandan, roll 31 and tanusree roll 39, alins'
-pattern = '\bhell'
-replace = '43'
+pattern = '[aeiou]'
+# replace = '43'
 result = re.findall(pattern, string)
+length = len(result)
 print(result)
+print(length)
+count = 0
+for i in string:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        count += 1
+print(count)
